ae4b72972d64190c9cfba29d9684121fc4a2beda
ludhill <lucasdenner.hs@live.com>
2026-05-14 02:34:44 -0300

    Corrigindo erros 6

--- src/test/java/br/org/edu/ifrn/LojaCarro/LojaCarroApplicationTests.java
+++ src/test/java/br/org/edu/ifrn/LojaCarro/LojaCarroApplicationTests.java
@@ -2,9 +2,11 @@ package br.org.edu.ifrn.LojaCarro;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LojaCarroApplicationTests {
 
     @Test
