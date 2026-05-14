8748f4fd43e6df53e3328cf80564ae7d9ae05574
ludhill <lucasdenner.hs@live.com>
2026-05-14 01:25:38 -0300

    Corrigindo erros

--- src/test/java/br/org/edu/ifrn/LojaCarro/LojaCarroApplicationTests.java
+++ src/test/java/br/org/edu/ifrn/LojaCarro/LojaCarroApplicationTests.java
@@ -9,13 +9,12 @@ class LojaCarroApplicationTests {
 
     @Test
     void contextLoads() {
-        // Testa se o Spring sobe com o H2 (application-test.properties)
     }
 
     @Test
     void simularFalhaItem3() {
-        // ITEM 3: Force a falha aqui para o vídeo
-        assertEquals("Sucesso", "Falha", "Forçando erro para o pipeline do GitHub Actions");
-        //assertEquals("Sucesso", "Sucesso");
+
+        assertEquals("Sucesso", "Sucesso");
+
     }
 }
