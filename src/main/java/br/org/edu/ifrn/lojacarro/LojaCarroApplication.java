66397395f007c53f5579a6ac9759bb61b4faa88d
20241148060038 <l.denner@escolar.ifrn.edu.br>
2026-09-08 08:41:00 -0300

    feat: aula 3, subir para aws e acessar banco

--- src/main/java/br/org/edu/ifrn/lojacarro/LojaCarroApplication.java
+++ src/main/java/br/org/edu/ifrn/lojacarro/LojaCarroApplication.java
@@ -1,47 +1,27 @@
 package br.org.edu.ifrn.lojacarro;
 
-import br.org.edu.ifrn.lojacarro.model.Usuario;
-import br.org.edu.ifrn.lojacarro.repository.UsuarioRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-public class LojaCarroApplication {
+public class LojaCarroApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LojaCarroApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaCarroApplication.class, args);
 	}
 
-
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	CommandLineRunner initDatabase(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
-		return args -> {
-
-			if (usuarioRepository.findByUsername("root").isEmpty()) {
-				Usuario root = new Usuario();
-				root.setUsername("root");
-				root.setPassword(passwordEncoder.encode("root"));
-				root.setRole("ROLE_GERENTE");
-				usuarioRepository.save(root);
-			}
-
-
-			if (usuarioRepository.findByUsername("vendedor1").isEmpty()) {
-				Usuario vendedor = new Usuario();
-				vendedor.setUsername("vendedor1");
-				vendedor.setPassword(passwordEncoder.encode("123456"));
-				vendedor.setRole("ROLE_VENDEDOR");
-				usuarioRepository.save(vendedor);
-			}
-		};
 	}
 }
